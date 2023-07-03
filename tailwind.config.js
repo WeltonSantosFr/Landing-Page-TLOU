7b6e4d0a584819bb03916493e93f05cc2aeec61e
=Welton Santos <weltonsantosfr@gmail.com>
2023-07-03 12:47:23 -0300

    fix:images in vercel deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,11 @@ export default {
     extend: {
       backgroundImage: {
         "home-image":
-          "url('static/images/The-Last-Of-Us-HBO-Wallpaper-13 1.png')",
-        "survival-image": "url('static/images/survivalimage.png')",
-        "relationship-image": "url('static/images/the-last-of-us1.png')",
+          "url('assets/static/images/The-Last-Of-Us-HBO-Wallpaper-13 1.png')",
+        "survival-image": "url('assets/static/images/survivalimage.png')",
+        "relationship-image": "url('assets/static/images/the-last-of-us1.png')",
         "adventure-image":
-          "url('static/images/FF3P7D3VJ5DZ5FGALN2YYQU3ZM1.png')",
+          "url('assets/static/images/FF3P7D3VJ5DZ5FGALN2YYQU3ZM1.png')",
       },
     },
   },
